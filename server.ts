0885d713e83b4691391b318df8f475a677db356a
Inmersys <developer@inmersys.com>
2021-11-14 19:17:13 -0600

    se agrego tsc-watch

--- server.ts
+++ server.ts
@@ -1,28 +1,48 @@
 import express from 'express'
 import authRouter from './src/routes/auth.route'
 import userRouter from './src/routes/user.route'
-import { createConnection } from 'typeorm'
+import { Connection, createConnection } from 'typeorm'
 
-const app = express()
-const port = 3000
-
-app.use('/api/auth', authRouter)
-app.use('/api/user', userRouter)
-
-const main = async () => {
-  try {
-    const connection = await createConnection()
-    if (!connection.isConnected) {
-      throw new Error("DataBase is disconected"); 
-    }else{
-      console.log("DataBase is connected"); 
+class Server {
+  app
+  connection: Connection
+  endpoints = {
+    baseRoute: '',
+    authentication: '/api/auth',
+    user: '/api/user'
+  }
+  port: number
+  constructor (init: { port: number }) {
+    try {
+      this.app = express()
+      this.port = init.port
+      this.lauchDataBase()
+      this.lauchRoutes()
+      this.lauchServer()
+    } catch (error) {
+      console.log(`Error: ${error}`)
     }
-    app.listen(port, () => {
-      console.log("================================")
-      return console.log(`Server is listening on ${port}`)
+  }
+  private async lauchDataBase () {
+    this.connection = await createConnection()
+    if (!this.connection.isConnected) {
+      throw new Error('DataBase is disconected')
+    } else {
+      console.log('DataBase is connected')
+    }
+  }
+  private lauchRoutes () {
+    this.app.use(this.endpoints.authentication, authRouter)
+    this.app.use(this.endpoints.user, userRouter)
+  }
+
+  lauchServer () {
+    this.app.listen(this.port, () => {
+      return console.log(`Server is listening on ${this.port}`)
     })
-  } catch (error) {
-    console.log(`Error: ${error}`)
   }
 }
-main()
+
+const port:number = parseInt(process.env.PORT || '3000') 
+
+const server = new Server({port})
--- types/responses.ts
+++
@@ -1,5 +0,0 @@
-exports.MSG_user404 = "El usuario no existe"
-exports.MSG_pass401 = "Password incorrecto"
-exports.MSG_auth200 = "Auth ok"
-exports.MSG_ERRORAUTH = "Ocurrio un error al autenticar"
-exports.MSG_UNAUTHORIZED = "No tienes permitida esta acción"
