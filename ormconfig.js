b0495dcd1ab3b28af4a06d3d19fced88dc23ae71
isaias <isaias.ch4vez@gmail.com>
2022-01-21 01:45:05 -0600

    configuracion base de datos

--- ormconfig.js
+++ ormconfig.js
@@ -1,22 +1,24 @@
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
-  port: '5432',
-  username: 'postgres',
-  password: 'root',
-  database: 'bazarutm',
+  host: 'bazarutm-do-user-10684883-0.b.db.ondigitalocean.com',
+  port: '25060',
+  username: 'doadmin',
+  password: 'wAMVrsYA0Xlpzkum',
+  
+  database: 'defaultdb',
   entities: ['./dist/src/modules/**/*.entity.js'],
   migrationTableName: 'migrations',
   migrations: [
       "src/migration/**/*.ts"
-   ],  //ssl: true,
+   ],  //
+   ssl: true,
   extra: {
     max: 25,
     min: 1,
-    //ssl: true,
-    //ssl: {
-    //  rejectUnauthorized: false,
-    //},
+    ssl: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
   cli: {
     migrationsDir: './src/database/migrations',
